43ba9b89a01b9d16e5ac68019052675c4d9787cd
ArturOle <artur.oleksinski99@gmail.com>
2024-10-17 15:36:26 +0200

    list_all_in_bucket tests

--- test/test_buck.py
+++ test/test_buck.py
@@ -1,6 +1,8 @@
 from buck import buck_aws
 
 
+# Credentials check tests
+
 def test_check_credentials_correct(mocker):
     session_mock = mocker.patch("buck.buck_aws.boto3.Session")
     mocker.patch("os.getenv", return_value="key")
@@ -29,3 +31,31 @@ def test_check_credentials_incorrect(mocker):
         assert str(e) == "AWS_SECRET_ACCESS_KEY not set"
     else:
         assert False
+
+
+# boto tests
+
+
+def test_list_all_in_bucket_correct(mocker):
+    s3_mock = mocker.MagicMock()
+    s3_mock.list_objects_v2.return_value = {
+        "Contents": [{"Key": "file1"}, {"Key": "file2"}]
+    }
+    mocker.patch("buck.buck_aws.boto3.Session.client", return_value=s3_mock)
+
+    result = buck_aws.list_all_in_bucket()
+    assert result == ["file1", "file2"]
+
+    s3_mock.list_objects_v2.assert_called_once_with(
+        Bucket="developer-task2",
+        Prefix="TIE-sa"
+    )
+
+
+def test_list_all_in_bucket_no_files(mocker):
+    s3_mock = mocker.MagicMock()
+    s3_mock.list_objects_v2.return_value = {}
+    mocker.patch("buck.buck_aws.boto3.Session.client", return_value=s3_mock)
+
+    result = buck_aws.list_all_in_bucket()
+    assert result is None
