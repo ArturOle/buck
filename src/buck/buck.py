5e961f6db29c8b614f58326e7da42c38ef8fdacb
ArturOle <artur.oleksinski99@gmail.com>
2024-10-17 13:57:46 +0200

    The project frame with raw/disabled implementations

--- src/buck/buck.py
+++ src/buck/buck.py
@@ -1,19 +1,28 @@
 import os
 import boto3
+import re
 
 
 def check_credentials(fucntion):
     """Check env variables for credentials"""
 
     def wrapper(*args, **kwargs):
-        if not os.getenv('AWS_ACCESS_KEY_ID'):
+        aws_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+        aws_secret_key = os.getenv('AWS_ACCESS_KEY')
+
+        if not aws_key_id:
             print('AWS_ACCESS_KEY_ID not set')
             exit(1)
-        if not os.getenv('AWS_ACCESS_KEY'):
-            print('AWS_ACCESS_KEY not set')
+        if not aws_secret_key:
+            print('AWS_SECRET_ACCESS_KEY not set')
             exit(1)
 
-        return fucntion(*args, **kwargs)
+        session = boto3.Session(
+            aws_access_key_id=aws_key_id,
+            aws_secret_access_key=aws_secret_key
+        )
+
+        return fucntion(*args, **kwargs, _session=session)
 
     return wrapper
 
@@ -21,14 +30,63 @@ def check_credentials(fucntion):
 @check_credentials
 def list_all_in_bucket(
         bucket_name: str = "developer-task",
-        directory: str = "TIE-sa"
+        directory: str = " TIE-sa",
+        _session: boto3.Session = None
 ):
     """List all files in bucket under directory"""
-    s3 = boto3.client('s3')
-    response = s3.list_objects_v2(Bucket=bucket_name, Prefix=directory)
-    return response['Contents']
+    # s3 = _session.client('s3')
+    # response = s3.list_objects_v2(Bucket=bucket_name, Prefix=directory)
+    # return [obj.key for obj in response]
+    return ["files"]
 
 
+@check_credentials
+def find_in_bucket(
+        bucket_name: str = "developer-task",
+        directory: str = "TIE-sa",
+        pattern: str = ".*",
+        _session: boto3.Session = None
+):
+    """Find file in bucket under directory"""
+    # s3 = boto3.client('s3')
+    # response = s3.list_objects_v2(Bucket=bucket_name, Prefix=directory)
+    # files = response['Contents']
+    # for file in files:
+    #     if re.search(pattern, file['Key']):
+    #         return file['Key']
+    # return None
+    return ["files"]
+
+
+@check_credentials
+def upload_file_to_bucket(
+        file_path: str,
+        bucket_name: str = "developer-task",
+        directory: str = "TIE-sa",
+        _session: boto3.Session = None
+):
+    """Upload file to bucket under directory"""
+    s3 = _session.resource('s3')
+    s3.meta.client.upload_file(
+        file_path,
+        bucket_name,
+        f"{directory}/{os.path.basename(file_path)}"
+    )
+
+
+@check_credentials
+def delete_matching_files(
+        bucket_name: str = "developer-task",
+        directory: str = "TIE-sa",
+        pattern: str = ".*",
+        _session: boto3.Session = None
+):
+    """Delete files in bucket under directory that match pattern"""
+    s3 = _session.resource('s3')
+    bucket = s3.Bucket(bucket_name)
+    for obj in bucket.objects.filter(Prefix=directory):
+        if re.search(pattern, obj.key):
+            obj.delete()
 
 
 if __name__ == '__main__':
--- src/buck/cli.py
+++ src/buck/cli.py
@@ -5,12 +5,52 @@ import buck
 
 
 @click.group()
-def cli():
-    pass
-
-
-@cli.command()
 @click.option('--bucket_name', default='developer-task', help='Bucket name')
 @click.option('--directory', default='TIE-sa', help='Directory')
-def list_all(bucket_name, directory):
-    return buck.list_all_in_bucket(bucket_name, directory)
+@click.pass_context
+def cli(ctx, directory, bucket_name):
+    ctx.obj["bucket_name"] = bucket_name
+    ctx.obj["directory"] = directory
+
+
+@cli.command("list_all")
+@click.pass_context
+def list_all(ctx):
+    print(buck.list_all_in_bucket(ctx.obj["bucket_name"], ctx.obj["directory"]))
+
+
+@cli.command("find")
+@click.option('--pattern', default='.*', help='Pattern')
+@click.pass_context
+def find(ctx, pattern):
+    return buck.find_in_bucket(
+        ctx.obj["bucket_name"],
+        ctx.obj["directory"],
+        pattern
+    )
+
+
+@cli.command("upload")
+@click.option('--file_path', help='File path')
+@click.pass_context
+def upload(ctx, file_path):
+    return buck.upload_file_to_bucket(
+        file_path,
+        ctx.obj["bucket_name"],
+        ctx.obj["directory"]
+    )
+
+
+@cli.command("delete")
+@click.option('--pattern', default='.*', help='Pattern')
+@click.pass_context
+def delete(ctx, pattern):
+    return buck.delete_matching_files(
+        ctx.obj["bucket_name"],
+        ctx.obj["directory"],
+        pattern
+    )
+
+
+if __name__ == '__main__':
+    cli(obj={})
